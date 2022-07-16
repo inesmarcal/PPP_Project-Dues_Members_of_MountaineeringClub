33f3c77bb054059d0b24d2cdc4e32b5719d48423
inesmarcal <inesmarcal@student.dei.uc.pt>
2022-07-16 14:21:30 +0100

    Delete CLion Folder

--- #clion_project/Project/mystring.h
+++
@@ -1,9 +0,0 @@
-#ifndef PROJETO_MONTANHISMO_MYSTRING_H
-#define PROJETO_MONTANHISMO_MYSTRING_H
-
-//Função à parte que permite remover os espaços em branco (carateres espaço e tabulação)
-// que se encontram no início de uma string.
-
-char *ltrim(char*);
-
-#endif //PROJETO_MONTANHISMO_MYSTRING_H
